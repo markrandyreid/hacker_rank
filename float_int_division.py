8aeecc728d5d171eb7115f4610748fe7a72213da
Mark Randy Reid <reid.233@buckeyemail.osu.edu>
2024-01-28 19:06:13 +0000

    committed

---
+++ float_int_division.py
@@ -0,0 +1,7 @@
+#given user input of two strings, print integer and float division of those strings
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(f'{a//b}\n{a/b}')
